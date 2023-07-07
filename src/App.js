611a6324deac524c127cbfc7aa15adef8ff58ca3
hopeinjavascript <hopeinjavascript@gmail.com>
2023-07-07 16:26:22 +0530

    style: home page style change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header>
         <p>Welcome Home</p>
       </header>
     </div>
